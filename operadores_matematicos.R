e8e0fa412b899650f17380d2630938067fc9543b
joaopaulo418 <joaopaulosantos418@gmail.com>
2024-05-30 09:29:18 -0300

    Operadores matemáticos em R
    Alguns exemplos de uso de operadores matemáticos na linguagem R.

---
+++ operadores_matematicos.R
@@ -0,0 +1,56 @@
+#A combinação de ctrl+ENTER executa uma linha de código aqui no RStudio
+
+#Aspas duplas ou simples fazem a mesma coisa igual python
+print("Olá mundo!")
+print('Olá mundo!')
+
+#Potenciação
+4**2
+#ou
+4^2
+
+#Raiz quadrada
+sqrt(81)
+
+#Raiz cubica
+125**(1/3)
+
+#Módulo
+abs(-10)
+
+#Fatorial
+factorial(3)#3! = 3 * 2 * 1
+
+#Logaritmo(base 2 e base 10)
+log10(100)
+log2(8)
+
+#Exponencial
+#e^1
+exp(1)#Número de euler e = 2,71828
+
+#logaritmo natural(base é o número de euler)
+log(2.718)
+
+#pi
+pi
+
+#Importante: no R, o ângulo está em radianos
+#pi radianos  = 180 graus
+
+#seno de 30 graus
+sin(pi/6)
+#inverso
+asin(0.5)
+#Cosseno e tangente de 45 graus
+cos(pi/4)
+tan(pi/4)
+
+#Notação científica
+2e32#Significa (2 * (10^32))
+
+getRversion()
+
+
+
+
